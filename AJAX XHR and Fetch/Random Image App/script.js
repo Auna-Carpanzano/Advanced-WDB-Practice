0b22aacbe7ede87643e38d493b81e96cbe82695c
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-22 18:20:36 -0700

    Add open method to XHR that is a GET request to an api url, add send method to XHR

--- AJAX XHR and Fetch/Random Image App/script.js
+++ AJAX XHR and Fetch/Random Image App/script.js
@@ -2,4 +2,6 @@ var btn = document.querySelector("#btn");
 
 btn.addEventListener("click", function() {
   var XHR = XMLHttpRequest();
+  XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+  XHR.send();
 });
