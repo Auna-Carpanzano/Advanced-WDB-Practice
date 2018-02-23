5be6d5e553a7695515a20abfe9009f2b26ea1c71
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-22 21:07:49 -0700

    Add var avatar, fullname, username, email, and city that gets each element by id in document

--- AJAX XHR and Fetch/Random User App/script.js
+++ AJAX XHR and Fetch/Random User App/script.js
@@ -1,1 +1,6 @@
 var url = "https://randomuser.me/api/";
+var avatar = document.getElementById("avatar");
+var fullname = document.getElementById("fullname");
+var username = document.getElementById("username");
+var email = document.getElementById("email");
+var city = document.getElementById("city");
