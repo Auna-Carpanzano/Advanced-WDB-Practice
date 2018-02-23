2cc03a466474fc21169d66a51f92413576876a40
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-22 18:23:45 -0700

    Add if statement to check if the readyState is 4 and the code status is 200, then console.log responseText

--- AJAX XHR and Fetch/Random Image App/script.js
+++ AJAX XHR and Fetch/Random Image App/script.js
@@ -4,7 +4,9 @@ btn.addEventListener("click", function() {
   var XHR = XMLHttpRequest();
   
   XHR.onreadystatechange = function() {
-    
+    if (XHR.readyState == 4 && XHR.status == 200) {
+      console.log(XHR.responseText);
+    }
   }
   
   XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
