a270e7f3dfa20c0b6cedeffc199dc487c652ebff
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-22 21:46:04 -0700

    Change innerText of username with username data from api

--- AJAX XHR and Fetch/Random User App/script.js
+++ AJAX XHR and Fetch/Random User App/script.js
@@ -25,4 +25,5 @@ function updateProfile (data) {
   var fullname = data.name.first + " " + data.name.last;
   fullnameDisplay.innerText = fullname;
   avatarDisplay.src = data.picture.medium;
+  username.innerText = data.login.username;
 }
