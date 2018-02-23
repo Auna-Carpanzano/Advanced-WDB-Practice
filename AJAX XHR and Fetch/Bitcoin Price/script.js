7c1c95525aeda53d65291c4a26b560ebf2409422
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-22 19:25:42 -0700

    Add var rate that parses JSON XHR responseText of rate in USD, set innerHTML of price to be the result of var rate

--- AJAX XHR and Fetch/Bitcoin Price/script.js
+++ AJAX XHR and Fetch/Bitcoin Price/script.js
@@ -2,9 +2,11 @@ var btn = document.querySelector("#btn");
 
 btn.addEventListener("click", function() {
   var XHR = new XMLHttpRequest();
+  
   XHR.onreadystatechange = function() {
     if (XHR.readyState == 4 && XHR.status == 200) {
-    
+      var rate = JSON.parse(XHR.responseText).bpi.USD.rate;
+      price.innerHTML = rate;
     }
   }
   
