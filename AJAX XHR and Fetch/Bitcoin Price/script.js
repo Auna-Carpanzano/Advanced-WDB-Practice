c2d64c34678140082cd1c8a1f373b0570bed1ee4
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-22 19:06:52 -0700

    Add onreadystatechange method to XHR that checks is readyState is 4 and status is 200

--- AJAX XHR and Fetch/Bitcoin Price/script.js
+++ AJAX XHR and Fetch/Bitcoin Price/script.js
@@ -2,6 +2,12 @@ var btn = document.querySelector("#btn");
 
 btn.addEventListener("click", function() {
   var XHR = new XMLHttpRequest();
+  XHR.onreadystatechange = function() {
+    if (XHR.readyState == 4 && XHR.status == 200) {
+    
+    }
+  }
+  
   XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
   XHR.send();
 });
