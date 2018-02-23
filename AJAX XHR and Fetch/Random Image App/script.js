2f8147707efde6e9a449b428566950eb63fdb911
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-22 18:21:44 -0700

    Add onreadystatechange method to XHR that runs a function

--- AJAX XHR and Fetch/Random Image App/script.js
+++ AJAX XHR and Fetch/Random Image App/script.js
@@ -2,6 +2,11 @@ var btn = document.querySelector("#btn");
 
 btn.addEventListener("click", function() {
   var XHR = XMLHttpRequest();
+  
+  XHR.onreadystatechange = function() {
+    
+  }
+  
   XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
   XHR.send();
 });
