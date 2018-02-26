01864927bbfd8e61f2f4e69d9f8a71a7c5bf098c
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-25 20:26:29 -0700

    Add current value of i to newStr on each iteration

--- Object Oriented Programming/script.js
+++ Object Oriented Programming/script.js
@@ -68,6 +68,6 @@ Array.prototype.map = function(callback) {
 String.prototype.reverse = function() {
   var newStr = '';
   for (var i = this.length -1; i >= 0; i--) {
-    
+    newStr += this[i];
   }
 }
