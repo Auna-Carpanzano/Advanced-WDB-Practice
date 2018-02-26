adfbc3f105b5f9848fa1c3736ede6c95cf0f2042
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-25 18:28:31 -0700

    Create function flip that is the same as function bind except use .slice method on allArgs, and .reverse method on allArgs to return fn

--- Closures/script.js
+++ Closures/script.js
@@ -73,3 +73,12 @@ function bind(fn, thisArg){
     return fn.apply(thisArg, allArgs);
   };
 }
+
+function flip(fn, thisArg){
+  var outerArgs = [].slice.call(arguments, 2);
+  return function() {
+    var innerArgs = [].slice.call(arguments);
+    var allArgs = outerArgs.concat(innerArgs).slice(0, fn.length);
+    return fn.apply(thisArg, allArgs.reverse());
+  };
+}
