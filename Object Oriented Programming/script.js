263b8222fb45a993971851118683da6bbd27c8ed
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-25 19:52:49 -0700

    Create constructor function for Person with firstName, lastName, favoriteColor, and favoriteNumber using the "this" keyword

--- Object Oriented Programming/script.js
+++ Object Oriented Programming/script.js
@@ -38,5 +38,9 @@ Vehicle.prototype.honk = function() {
   }
 }
 
-
-
+function Person(firstName, lastName, favoriteColor, favoriteNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.favoriteColor = favoriteColor;
+  this.favoriteNumber = favoriteNumber;
+}
