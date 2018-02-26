7df26e987c8b8515522cef004b66982bab83b3b0
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-25 19:21:09 -0700

    Use the "this" keyword to set each parameter to the newly created object

--- Object Oriented Programming/script.js
+++ Object Oriented Programming/script.js
@@ -8,5 +8,8 @@ function Dog(name, age) {
 }
 
 function Person(firstName, lastName, favoriteColor, favoriteNumber) {
-
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.favoriteColor = favoriteColor;
+  this.favoriteNumber = favoriteNumber;
 }
