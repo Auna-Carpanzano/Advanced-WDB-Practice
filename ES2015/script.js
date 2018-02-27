859ab5e6466d410483d936a793a9b634976f6719
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-27 13:12:57 -0700

    Refactor function mapFilterAndReduce with arrow functions

--- ES2015/script.js
+++ ES2015/script.js
@@ -19,3 +19,20 @@ let tripleAndFilter = arr => arr.map(value => value * 3).filter(value => value %
 //}
 
 let doubleOddNumbers = arr => arr.filter(val => val % 2 !== 0).map(val => val * 2);
+
+//function mapFilterAndReduce(arr){
+//  return arr.map(function(val){
+//    return val.firstName
+//  }).filter(function(val){
+//    return val.length < 5;
+//  }).reduce(function(acc,next){
+//    acc[next] = next.length
+//    return acc;
+//  }, {})
+//}
+
+let mapFilterAndReduce = arr => arr.map(val => val.firstName).filter(val => val.length < 5)
+  .reduce((acc,next) => {
+    acc[next] = next.length;
+    return acc;
+  }, {});
