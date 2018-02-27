b98e7cc952a41ba90a8eb599f760042a925f5251
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-27 16:06:55 -0700

    Add function joinArrays that has a variable number of parameters and returns the concatenated array parameters

--- ES2015/Rest and Spread/script.js
+++ ES2015/Rest and Spread/script.js
@@ -3,8 +3,12 @@ function smallestValue(...nums) {
   return Math.min(...nums);
 }
 
-function placeInMiddle(arr, vals){
+function placeInMiddle(arr, vals) {
   let mid = Math.floor(arr.length/2);
   arr.splice(mid, 0, ...vals);
   return arr;
+}
+
+function joinArrays(...args) {
+  return args.reduce((acc, next) => acc.concat(next), []);
 }
