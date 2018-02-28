e7ed50da9368185ef551db0e42c9ed566e2d89bb
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-27 17:44:34 -0700

    Add function printFullName that descructurs object directly from parameters, returns a template string

--- ES2015/Destructuring/script.js
+++ ES2015/Destructuring/script.js
@@ -3,3 +3,7 @@ function displayStudentInfo(obj) {
   var {first, last} = obj;
   return `Your full name is ${first} ${last}`;
 }
+
+function printFullName({first, last}) {
+  return `Your full name is ${first} ${last}`;
+}
