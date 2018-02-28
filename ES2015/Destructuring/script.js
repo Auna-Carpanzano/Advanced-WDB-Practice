590690aaa1ddca0bcb8a53f9382f32fa3c820ca1
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-27 17:58:07 -0700

    Add function createStudent that sets default parameters as object, add if/else statements to concatenate var start with strings for different parameters

--- ES2015/Destructuring/script.js
+++ ES2015/Destructuring/script.js
@@ -7,3 +7,17 @@ function displayStudentInfo(obj) {
 function printFullName({first, last}) {
   return `Your full name is ${first} ${last}`;
 }
+
+function createStudent({likesES2015 = true, likesJavaScript = true} = {}) {
+  var start = "The student ";
+  if (likesES2015 && likesJavaScript) {
+    start += "likes JavaScript and ES2015";
+  } else if (likesJavaScript) {
+    start += "The student likes JavaScript!";
+  } else if (likesES2015) {
+    start += "The student likesES2015!";
+  } else {
+    start += "The student does not like much...";
+  }
+  return start;
+}
