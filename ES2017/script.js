f9e301b8a3471df5082bed3a05b2d35560abaa60
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-28 18:11:51 -0700

    Return a template string that accesses max.name and max.followers

--- ES2017/script.js
+++ ES2017/script.js
@@ -3,4 +3,5 @@ async function hasMostFollowers(...usernames) {
   let urls = usernames.map(username => $.getJSON(baseURL + username));
   let results = await Promise.all(urls);
   let max = results.sort((a, b) => a.followers < b.followers)[0];
+  return `${max.name} has the most followers with ${max.followers}`
 }
