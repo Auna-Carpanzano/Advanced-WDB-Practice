1ff3ac394663d58ea40f8307662823ead45c9ce4
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-28 18:08:16 -0700

    Add let results that is the result of awaiting a Promise to let urls getJSON

--- ES2017/script.js
+++ ES2017/script.js
@@ -1,4 +1,5 @@
 async function hasMostFollowers(...usernames) {
   let baseURL = "https://api.github.com/users/"
   let urls = usernames.map(username => $.getJSON(baseURL + username));
+  let results = await Promise.all(urls);
 }
