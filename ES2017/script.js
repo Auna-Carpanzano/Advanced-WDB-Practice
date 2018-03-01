3facb1e2ca3694e733f20fc630f525127eee6b29
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-28 18:10:34 -0700

    Add let max that uses sort method on results to compare and get data for the username with the most followers

--- ES2017/script.js
+++ ES2017/script.js
@@ -2,4 +2,5 @@ async function hasMostFollowers(...usernames) {
   let baseURL = "https://api.github.com/users/"
   let urls = usernames.map(username => $.getJSON(baseURL + username));
   let results = await Promise.all(urls);
+  let max = results.sort((a, b) => a.followers < b.followers)[0];
 }
