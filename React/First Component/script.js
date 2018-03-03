641abb3fc333f2458e4e960f111165d24cdd482c
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 16:30:58 -0700

    Refactor ReactDOMFactories code with JSX code

--- React/First Component/script.js
+++ React/First Component/script.js
@@ -1,11 +1,11 @@
 class Pet extends React.Component {
   render() {
-    const h2 = ReactDOMFactories.h2(null, "Moxie");
-    const img = ReactDOMFactories.img({
-      src: "https://images.unsplash.com/photo-1511876849877-f8d0d953d2e0?ixlib=rb-0.3.5&s=4488ff58aa49f9fc89091a95f9c4fa4d&auto=format&fit=crop&w=1050&q=80",
-      alt: "Moxie the cat"
-    });
-    return ReactDOMFactories.div(null, h2, img);
+    return (
+      <div>
+        <h2>Moxie</h2>
+        <img src="https://images.unsplash.com/photo-1511876849877-f8d0d953d2e0?ixlib=rb-0.3.5&s=4488ff58aa49f9fc89091a95f9c4fa4d&auto=format&fit=crop&w=1050&q=80" alt="Moxie the cat" />
+      </div>
+    )
   }
 }
-ReactDOM.render(React.createElement(Pet), document.getElementById("app"));
+ReactDOM.render(<Pet />, document.getElementById("app"));
