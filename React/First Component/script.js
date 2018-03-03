087dadbb13f238ae94af3df2b67344cd02722536
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 16:42:52 -0700

    Add className for div, h2, and img

--- React/First Component/script.js
+++ React/First Component/script.js
@@ -1,9 +1,9 @@
 class Pet extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Moxie</h2>
-        <img src="https://images.unsplash.com/photo-1511876849877-f8d0d953d2e0?ixlib=rb-0.3.5&s=4488ff58aa49f9fc89091a95f9c4fa4d&auto=format&fit=crop&w=1050&q=80" alt="Moxie the cat" />
+      <div className="card">
+        <h2 className="name">Moxie</h2>
+        <img src="https://images.unsplash.com/photo-1511876849877-f8d0d953d2e0?ixlib=rb-0.3.5&s=4488ff58aa49f9fc89091a95f9c4fa4d&auto=format&fit=crop&w=1050&q=80" alt="Moxie the cat" className="img" />
       </div>
     )
   }
