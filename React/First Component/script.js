dc9c3b4064e3862c0825cc01d740084a72feb8e1
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 16:07:12 -0700

    Create img with src and alt attributes

--- React/First Component/script.js
+++ React/First Component/script.js
@@ -1,5 +1,9 @@
 class Pet extends React.Component {
   render() {
     const h2 = ReactDOMFactories.h2(null, "Moxie");
+    const img = ReactDOMFactories.img({
+      src: "https://images.unsplash.com/photo-1511876849877-f8d0d953d2e0?ixlib=rb-0.3.5&s=4488ff58aa49f9fc89091a95f9c4fa4d&auto=format&fit=crop&w=1050&q=80",
+      alt: "Moxie the cat"
+    });
   }
 }
