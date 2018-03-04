a4de45b126552d14b19807bd9f79874b9e13bb39
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 20:06:27 -0700

    Destructure this.props.ingredients by adding const {ingredients} that maps through ingredients and creates a list items with key of index for each ingredient

--- React/Create React App/recipe-props-starter/src/Recipe.js
+++ React/Create React App/recipe-props-starter/src/Recipe.js
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 class Recipe extends Component {
   render() {
     const {title} = this.props;
+    const {ingredients} = this.props.ingredients.map((ing, index) => (
+      <li key={index}>{ing}</li>
+    ));
     return (
       <div>Recipe {title}</div>
     )
