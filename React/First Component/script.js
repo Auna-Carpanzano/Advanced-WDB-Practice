fc5440ac71d8d319e4e943fc2005caac2396f6ea
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 17:39:13 -0700

    Render HobbyList class in pet class

--- React/First Component/script.js
+++ React/First Component/script.js
@@ -19,6 +19,7 @@ class Pet extends React.Component {
         <h2 className="name">Moxie</h2>
         <img src="https://images.unsplash.com/photo-1511876849877-f8d0d953d2e0?ixlib=rb-0.3.5&s=4488ff58aa49f9fc89091a95f9c4fa4d&auto=format&fit=crop&w=1050&q=80" alt="Moxie the cat" className="img" />
       <h5 style={{fontSize: "2em"}}>Hobbies:</h5>
+      <HobbyList />
       </div>
     )
   }
