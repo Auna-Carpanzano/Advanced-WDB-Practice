ebbd04b24b3a7910ab75dbe5ac92ae87c58c8030
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 17:36:54 -0700

    Move const hobbies and ul from Pet class to HobbyList class

--- React/First Component/script.js
+++ React/First Component/script.js
@@ -1,23 +1,22 @@
 class HobbyList extends React.Component {
   render() {
-    
+    const hobbies = ["Sleeping", "Eating", "Cuddling"];
+    <ul>
+      {hobbies.map((h, i) => {
+        return <li key={i} style={liStyle}>{h}</li>
+      })}
+    </ul>
   }
 }
 
 class Pet extends React.Component {
   render() {
     const liStyle = {fontSize: "1.5em"};
-    const hobbies = ["Sleeping", "Eating", "Cuddling"];
     return (
       <div className="card">
         <h2 className="name">Moxie</h2>
         <img src="https://images.unsplash.com/photo-1511876849877-f8d0d953d2e0?ixlib=rb-0.3.5&s=4488ff58aa49f9fc89091a95f9c4fa4d&auto=format&fit=crop&w=1050&q=80" alt="Moxie the cat" className="img" />
       <h5 style={{fontSize: "2em"}}>Hobbies:</h5>
-      <ul>
-        {hobbies.map((h, i) => {
-          return <li key={i} style={liStyle}>{h}</li>
-      })}
-      </ul>
       </div>
     )
   }
