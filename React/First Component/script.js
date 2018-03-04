5c7439fcfcc5579a8a06705f3adcb200f25a4c49
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 17:30:51 -0700

    Refactor list items by using .map method on const hobbies to return each hobby in an li

--- React/First Component/script.js
+++ React/First Component/script.js
@@ -8,8 +8,9 @@ class Pet extends React.Component {
         <img src="https://images.unsplash.com/photo-1511876849877-f8d0d953d2e0?ixlib=rb-0.3.5&s=4488ff58aa49f9fc89091a95f9c4fa4d&auto=format&fit=crop&w=1050&q=80" alt="Moxie the cat" className="img" />
       <h5 style={{fontSize: "2em"}}>Hobbies:</h5>
       <ul>
-        <li style={liStyle}>Sleep</li>
-        <li style={liStyle}>Eat</li>
+        {hobbies.map(h => {
+          return <li style={liStyle}>{h}</li>
+      })}
       </ul>
       </div>
     )
