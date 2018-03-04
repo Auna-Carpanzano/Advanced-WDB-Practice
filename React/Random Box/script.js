4649a074799bd6f39ff2ce7201b4d5375ac96d03
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 17:23:28 -0700

    Return div with inline styles that use fontSize and backgroundColor consts

--- React/Random Box/script.js
+++ React/Random Box/script.js
@@ -2,5 +2,19 @@ class Box extends React.Component {
   render() {
     const fontSize = Math.floor(Math.random() * 80) + 20;
     const backgroundColor = ["black", "red", "blue", "green"][Math.floor(Math.random() * 4)];
+    return (
+      <div style={{
+        fontSize: `${fontSize}px`,
+        backgroundColor,
+        width: "500px", 
+        height: "200px", 
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white"
+      }}>
+        Random Box
+      </div>
+    )
   }
 }
