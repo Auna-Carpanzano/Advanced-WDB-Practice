4839da10d092e8ead082769ca6ec696b49a3a321
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 20:13:27 -0700

    Change const ingredients

--- React/Create React App/recipe-props-starter/src/Recipe.js
+++ React/Create React App/recipe-props-starter/src/Recipe.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Recipe extends Component {
   render() {
     const {title} = this.props;
-    const {ingredients} = this.props.ingredients.map((ing, index) => (
+    const ingredients = this.props.ingredients.map((ing, index) => (
       <li key={index}>{ing}</li>
     ));
     return (
