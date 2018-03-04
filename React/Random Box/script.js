365f0d52864e540feb5e507da6c6a7cad56c0530
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 17:22:39 -0700

    Add const backgroundColor that calculates index for an array of four colors with Math.floor and Math.random

--- React/Random Box/script.js
+++ React/Random Box/script.js
@@ -1,5 +1,6 @@
 class Box extends React.Component {
   render() {
     const fontSize = Math.floor(Math.random() * 80) + 20;
+    const backgroundColor = ["black", "red", "blue", "green"][Math.floor(Math.random() * 4)];
   }
 }
