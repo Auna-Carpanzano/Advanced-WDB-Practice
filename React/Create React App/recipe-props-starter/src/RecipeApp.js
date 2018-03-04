fed9f4e365b7e29e22316fcf8c57976c5b09790e
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 20:22:48 -0700

    Add instructions and img properties to Recipe, remove logo import

--- React/Create React App/recipe-props-starter/src/RecipeApp.js
+++ React/Create React App/recipe-props-starter/src/RecipeApp.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Recipe from "./Recipe"
 import './RecipeApp.css';
 
@@ -7,7 +6,11 @@ class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
-        <Recipe title="pasta" ingredients={["flour", "water"]}/>
+        <Recipe title="pasta" 
+                ingredients={["flour", "water"]}
+                instructions="Mix ingredients"
+                img="https://images.unsplash.com/photo-1518675219903-c682c4b16b1d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=28d9da11bbd83f4bab4cbeb1a8c6ad0b&auto=format&fit=crop&w=800&q=80"
+        />
       </div>
     );
   }
