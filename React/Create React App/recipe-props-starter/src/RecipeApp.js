bb3cc403661116971d7e27335ea0bee9c3ce9c18
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-03 21:43:25 -0700

    Remove single Recipe, render RecipeList

--- React/Create React App/recipe-props-starter/src/RecipeApp.js
+++ React/Create React App/recipe-props-starter/src/RecipeApp.js
@@ -9,11 +9,7 @@ class RecipeApp extends Component {
     return (
       <div className="App">
         <Navbar />
-        <Recipe title="pasta" 
-                ingredients={["flour", "water"]}
-                instructions="Mix ingredients"
-                img="https://images.unsplash.com/photo-1518675219903-c682c4b16b1d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=28d9da11bbd83f4bab4cbeb1a8c6ad0b&auto=format&fit=crop&w=800&q=80"
-        />
+        <RecipeList />
       </div>
     );
   }
