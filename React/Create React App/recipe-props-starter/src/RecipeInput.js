526669e7289e34b28422d77824306c61e7cad35f
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-05 18:11:57 -0700

    Add constructor, add states of title, instructions, ingredients, and img

--- React/Create React App/recipe-props-starter/src/RecipeInput.js
+++ React/Create React App/recipe-props-starter/src/RecipeInput.js
@@ -1,7 +1,15 @@
 import React, {Component} from "react";
 
 class RecipeInput extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      instructions: "",
+      ingredients: [""],
+      img: ""
+    }
+  }
 }
 
 export default RecipeInput;
