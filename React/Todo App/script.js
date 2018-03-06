58ce57c78eb45f7281e32aec5bf5db40b69f6222
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-05 17:26:44 -0700

    Add input with onChange event that updates setState of value

--- React/Todo App/script.js
+++ React/Todo App/script.js
@@ -25,6 +25,13 @@ class Form extends React.Component {
       <div>
         <h1>Todo App</h1>
         <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="newTodo"
+            placeholder="Add new todo..."
+            value={newTodo}
+            onChange={(e) => this.setState({[e.target.name]: e.target.value})}
+          />
         </form>
         <div>
           <ol>
